7ef7d222012b4a3f9700904cc297c0b87e3f41a9
JairoPacheco <48835989+JairoPacheco@users.noreply.github.com>
2019-09-09 18:52:06 -0600

    Add files via upload

---
+++ Classes/Classes/Report.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import Methods.ReportMethods;
+import Frames.FrameUser;
+
+/**
+ *
+ * @author yosin
+ */
+public class Report {
+    
+     
+    public static ReportMethods List;
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+        List= new ReportMethods();
+        FrameUser reportUser= new FrameUser();
+        reportUser.setVisible(true);
+        
+        
+    }
+    
+}
